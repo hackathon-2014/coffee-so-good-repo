c0d07d40b314a789d34c815eece7c1bbf7a75008
ChuckWoodraska <chuck.woodraska@gmail.com>
2014-08-23 11:32:58 -0400

    Shake implemented
    Shake for a message

--- Accelerometer/app/src/main/java/com/chuckwoodraska/accelerometer/app/MainActivity.java
+++ Accelerometer/app/src/main/java/com/chuckwoodraska/accelerometer/app/MainActivity.java
@@ -16,7 +16,11 @@ public class MainActivity extends Activity implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
 
-    TextView title, xvalue, yvalue, zvalue;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
+
+    TextView title, xvalue, yvalue, zvalue, speedvalue;
     RelativeLayout layout;
 
 
@@ -36,6 +40,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         xvalue =(TextView)findViewById(R.id.xval);
         yvalue =(TextView)findViewById(R.id.yval);
         zvalue =(TextView)findViewById(R.id.zval);
+        speedvalue =(TextView)findViewById(R.id.speed);
     }
 
     @Override
@@ -56,15 +61,37 @@ public class MainActivity extends Activity implements SensorEventListener {
     @Override
     public final void onSensorChanged(SensorEvent event)
     {
-        // Many sensors return 3 values, one for each axis.
-        float x = event.values[0];
-        float y = event.values[1];
-        float z = event.values[2];
-        //display values using TextView
-        title.setText(R.string.app_name);
-        xvalue.setText("X axis" + "\t\t" + x);
-        yvalue.setText("Y axis" + "\t\t" + y);
-        zvalue.setText("Z axis" + "\t\t" + z);
+        title.setText("Chuck");
+
+        Sensor mySensor = event.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    speedvalue.setText("Speeds" + "\t\t" + speed);
+                    onShake(speed);
+                }
+                xvalue.setText("X axis" + "\t\t" + x);
+                yvalue.setText("Y axis" + "\t\t" + y);
+                zvalue.setText("Z axis" + "\t\t" + z);
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+
+        }
     }
 
     @Override

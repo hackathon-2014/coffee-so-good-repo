3c4a74d225223cf0bcd9b85fcc57d79790eab305
ChuckWoodraska <chuck.woodraska@gmail.com>
2014-08-23 16:21:36 -0400

    Settings Menus
    Can change brush color and size

--- Accelerometer/app/src/main/java/com/chuckwoodraska/accelerometer/app/MainActivity.java
+++ Accelerometer/app/src/main/java/com/chuckwoodraska/accelerometer/app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.chuckwoodraska.accelerometer.app;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -14,23 +17,37 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Map;
 
 
 public class MainActivity extends Activity implements SensorEventListener {
     // DRAW THINGIES
     CustomDrawableView mCustomDrawableView = null;
     ShapeDrawable mDrawable = new ShapeDrawable();
-    Canvas savedCanvas = new Canvas();
+    public int color = Color.RED;
+    public int brush_size = 50;
     public static int xmove = 0;
     public static int ymove = 0;
     public ArrayList<RectF> paths = new ArrayList<RectF>();
+    public ArrayList<Integer> paths_color = new ArrayList<Integer>();
+    public ArrayList<Integer> paths_brushsize = new ArrayList<Integer>();
+    public String[] colors_array = { "RED", "BLUE", "GREEN", "YELLOW", "BLACK", "WHITE"};
+    public String[] brushsize_array = { "10","50","75"};
 
     // ACCELEROMETER STHUFF
     private SensorManager mSensorManager;
@@ -39,7 +56,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     // SHAKE VARS
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
-    private static final int SHAKE_THRESHOLD = 600;
+    private static final int SHAKE_THRESHOLD = 1000;
 
     // VIEW VARS
     TextView title, xvalue, yvalue, zvalue, speedvalue;
@@ -77,7 +94,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     public void onShake(float force) {
 
         // Do your stuff here
-
+        paths = new ArrayList<RectF>();
         // Called when Motion Detected
         Toast.makeText(getBaseContext(), "SHAKING IT!!!",
         Toast.LENGTH_SHORT).show();
@@ -114,9 +131,31 @@ public class MainActivity extends Activity implements SensorEventListener {
                 last_x = x;
                 last_y = y;
                 last_z = z;
+
+                // Add oval to canvas
                 Boolean tilt = false;
-                if(((int)x) > 2) {
+                if(((int)x) > 2 && ((int)y) > 2) {
+                    xmove += 5;
+                    ymove += 5;
+                    tilt = true;
+                }
+                else if(((int)x) > 2 && ((int)y) < -2) {
+                    xmove += 5;
+                    ymove -= 5;
+                    tilt = true;
+                }
+                else if(((int)x) > 2) {
                     xmove += 10;
+                    tilt = true;
+                }
+                else if(((int)x) < -2 && ((int)y) > 2) {
+                    xmove -= 5;
+                    ymove += 5;
+                    tilt = true;
+                }
+                else if(((int)x) < -2 && ((int)y) < -2) {
+                    xmove -= 5;
+                    ymove -= 5;
                     tilt = true;
                 }
                 else if(((int)x) < -2) {
@@ -132,8 +171,10 @@ public class MainActivity extends Activity implements SensorEventListener {
                     tilt = true;
                 }
                 if(tilt) {
-                    RectF oval = new RectF(ymove, xmove, ymove + 50, xmove + 50); // set bounds of rectangle
+                    RectF oval = new RectF(ymove, xmove, ymove + brush_size, xmove + brush_size); // set bounds of rectangle
                     paths.add(oval);
+                    paths_color.add(color);
+                    paths_brushsize.add(brush_size);
                 }
 
             }
@@ -143,15 +184,15 @@ public class MainActivity extends Activity implements SensorEventListener {
     public class CustomDrawableView extends View
     {
         // Change later for brush size
-        static final int width = 50;
-        static final int height = 50;
+        int width = brush_size;
+        int height = brush_size;
 
         public CustomDrawableView(Context context)
         {
             super(context);
 
             mDrawable = new ShapeDrawable(new OvalShape());
-            mDrawable.getPaint().setColor(Color.RED);
+            mDrawable.getPaint().setColor(color);
             mDrawable.setBounds(ymove, xmove, ymove + width, xmove + height);
         }
 
@@ -159,10 +200,92 @@ public class MainActivity extends Activity implements SensorEventListener {
         {
             Paint p = new Paint(); // set some paint options
             for(int i =0; i < paths.size(); i++){
-                p.setColor(Color.RED); // change later for color options
+                p.setStrokeWidth(brush_size);
+                p.setColor(paths_color.get(i)); // change later for color options
                 canvas.drawOval(paths.get(i), p);
             }
             invalidate();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.change_brushsize:
+                final Dialog brush_size_dialog = new Dialog(this);
+                brush_size_dialog.setTitle("Select level:");
+                brush_size_dialog.setContentView(R.layout.brush_size_setting);
+
+                final TextView brushsize_title = (TextView)brush_size_dialog.findViewById(R.id.brush_size_title);
+                final SeekBar brushsize = (SeekBar)brush_size_dialog.findViewById(R.id.brush_size_slider);
+
+                brushsize.setMax(100);
+                brushsize.setProgress(100);
+
+                brushsize.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    //change to progress
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        brushsize_title.setText(Integer.toString(progress));
+                    }
+                    //methods to implement but not necessary to amend
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {}
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {}
+                });
+
+                Button okBtn = (Button)brush_size_dialog.findViewById(R.id.brush_size_ok);
+
+                okBtn.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                    //respond to level
+                        brush_size= brushsize.getProgress();
+                        brush_size_dialog.dismiss();
+                    }
+                });
+
+                brush_size_dialog.show();
+
+
+
+
+//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                builder.setTitle("Pick Brush Size:")
+//                        .setItems(brushsize_array, new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                brush_size = Integer.parseInt(brushsize_array[which]);
+//                                // The 'which' argument contains the index position
+//                                // of the selected item
+//                            }
+//                        });
+//                builder.create();
+//                builder.show();
+                return true;
+            case R.id.change_color:
+                AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+                builder2.setTitle("Pick color:")
+                        .setItems(colors_array, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                color = Color.parseColor(colors_array[which]);
+                                // The 'which' argument contains the index position
+                                // of the selected item
+                            }
+                        });
+                builder2.create();
+                builder2.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
